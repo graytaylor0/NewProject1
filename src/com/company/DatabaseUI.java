a3c8bf623dd51d969c98de8f1cdfac96ca7954ef
lwcreel <landon.creel@icloud.com>
2019-10-10 11:18:31 -0500

    added GUI

---
+++ src/com/company/DatabaseUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseui;
+
+/**
+ *
+ * @author Landon Creel
+ */
+public class DatabaseUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        HomePage.main(args);
+    }
+    
+}
